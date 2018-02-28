7b4dae67c714e24c36b64aada9a10b36734da32c
Prakash Murthy <prakblr@yahoo.com>
2018-02-28 16:28:18 -0700

    If entry is compressed, uncompress value before storing.

--- lib/active_support/cache/mongoid-store.rb
+++ lib/active_support/cache/mongoid-store.rb
@@ -46,12 +46,18 @@ module ActiveSupport
         expires_in = entry.instance_variable_get(:@expires_in)
         cache_entry = Entry.find_or_initialize_by(key: key)
         race_condition_ttl = options[:race_condition_ttl]
+        compressed = entry.instance_variable_get(:@compressed)
 
         if race_condition_ttl && expires_in && expires_in > 0
           expires_in += race_condition_ttl
         end
 
         cache_entry.save!
+
+        if compressed
+          value = Marshal.load(Zlib::Inflate.inflate(value))
+        end
+
         cache_entry.update(data: Marshal.dump(value), created_at: created_at, expires_in: expires_in )
       end
 
